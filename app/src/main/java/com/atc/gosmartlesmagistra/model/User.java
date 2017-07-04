d91435aa2c6523b31a08a0b0ea54cd127d6a4d6d
Hendri <hendri.gnw@gmail.com>
2017-07-04 22:43:48 +0700

    class dummy User

---
+++ app/src/main/java/com/atc/gosmartlesmagistra/model/User.java
@@ -0,0 +1,17 @@
+package com.atc.gosmartlesmagistra.model;
+
+/**
+ * Created by hendrigunawan on 04/07/2017.
+ */
+
+public class User {
+
+    public String getToken()
+    {
+        return "";
+    }
+
+    public String getCode() {
+        return "";
+    }
+}
