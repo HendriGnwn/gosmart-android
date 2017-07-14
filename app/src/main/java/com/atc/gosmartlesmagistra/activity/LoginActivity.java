f57b83d6a3b8369ef726ca4d999544396fc5b2d4
Hendri <hendri.gnw@gmail.com>
2017-07-14 19:41:06 +0700

    update layouts

--- app/src/main/java/com/atc/gosmartlesmagistra/activity/LoginActivity.java
+++ app/src/main/java/com/atc/gosmartlesmagistra/activity/LoginActivity.java
@@ -151,6 +151,7 @@ public class LoginActivity extends AppCompatActivity {
         } else {
 
             launchDialog();
+            progressBar.setVisibility(View.VISIBLE);
             // Show a progress spinner, and kick off a background task to
             // perform the user login attempt.
             Retrofit retrofit = new Retrofit.Builder()
@@ -188,7 +189,7 @@ public class LoginActivity extends AppCompatActivity {
 
                 @Override
                 public void onFailure(Call<LoginSuccess> call, Throwable t) {
-                    Toast.makeText(LoginActivity.this, "Login is failed", Toast.LENGTH_LONG).show();
+                    Toast.makeText(LoginActivity.this, "Login is failed, please try again", Toast.LENGTH_LONG).show();
                     progressBar.setVisibility(View.INVISIBLE);
                 }
             });
